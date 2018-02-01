0a6870d7bb520a4fb00664bcbd7122d37d82d43c
JQTvor <JonnyHMI@yahoo.com>
2018-02-01 15:35:20 -0500

    first commit

---
+++ Lab9/Circle.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab9
+{
+    class Circle
+    {
+
+        private double radius;
+        private const double PI = Math.PI;
+
+        public Circle(double r)
+        {
+            radius = r;
+        }
+
+
+        public double GetCircumference()
+        {
+            double circum = 2 * PI * radius;
+            return circum;
+        }
+
+        public double GetArea()
+        {
+            double area = (radius * radius) * PI;
+            return area;
+        }
+
+        public string FormatCircumference()
+        {
+            double c = GetCircumference();
+            c = Math.Round(c, 2);
+            return c.ToString();
+        }
+
+        public string FormatArea()
+        {
+            double a = GetArea();
+            a = Math.Round(a, 2);
+            return a.ToString();
+        }
+
+
+
+
+    }
+}
---
+++ Lab9/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab9
+{
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            String answer = Console.ReadLine(); double r = Convert.ToDouble(answer);
+            Circle c = new Circle(r);
+
+            Console.WriteLine("The circumference of " + r + " is " + c.FormatCircumference() + "\nThe area of " + r + " is " + c.FormatArea());
+
+        }
+    }
+}
